c55ed0037adee47da4dc99666c76b4c22a5dfa5c
vanitha540 <rvanitha.babu@gmail.com>
2024-08-29 01:50:56 +0800

    shell

--- backend.sh
+++ backend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/expense"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$ (echo $0 | cut -d "." -f1)
+TIMESTAMP=$ (date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAM.log"
 mkdir -p $LOGS_FOLDER
-USERID=$(id -u)
+USERID=$ (id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
